e8cf13d979dcbfb61a46be25a2cf761d9f70222c
Dev Patel <devapatel0603@gmail.com>
2024-06-22 15:40:41 +0530

    Complete

--- dummy_backend/data/posts.js
+++ dummy_backend/data/posts.js
@@ -1,16 +1,17 @@
-const fs = require("node:fs/promises");
+const fs = require("fs");
 
 async function getStoredPosts() {
-    const rawFileContent = await fs.readFile("dummy_backend/posts.json", {
+    const rawFileContent = await fs.readFileSync("dummy_backend/posts.json", {
         encoding: "utf-8",
     });
+    console.log(data);
     const data = JSON.parse(rawFileContent);
     const storedPosts = data.posts ?? [];
     return storedPosts;
 }
 
 function storePosts(posts) {
-    return fs.writeFile(
+    return fs.writeFileSync(
         "dummy_backend/posts.json",
         JSON.stringify({ posts: posts || [] })
     );
