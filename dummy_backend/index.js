5bbd02855681cbc2e97a67f37a568180c6fef715
Dev Patel <devapatel0603@gmail.com>
2024-06-22 15:22:19 +0530

    Complete

--- dummy_backend/index.js
+++ dummy_backend/index.js
@@ -6,7 +6,7 @@ const app = express();
 const port = 8080;
 
 app.use(
-    cors({ origin: "https://post-it-beryl.vercel.app/", credentials: true })
+    cors({ origin: "https://post-it-beryl.vercel.app", credentials: true })
 );
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
