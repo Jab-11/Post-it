722d7e284ce319027a6fb99e76ec16a811e87c30
Dev Patel <devapatel0603@gmail.com>
2024-06-22 15:52:10 +0530

    Complete

--- dummy_backend/data/posts.js
+++ dummy_backend/data/posts.js
@@ -1,18 +1,29 @@
 const fs = require("fs");
+const path = require("path");
 
 async function getStoredPosts() {
-    const rawFileContent = await fs.readFileSync("dummy_backend/posts.json", {
-        encoding: "utf-8",
-    });
-    console.log(data);
-    const data = JSON.parse(rawFileContent);
-    const storedPosts = data.posts ?? [];
-    return storedPosts;
+    const filePath = path.join(__dirname, "dummy_backend/posts.json");
+
+    try {
+        const rawFileContent = await fs.readFileSync(filePath, {
+            encoding: "utf-8",
+        });
+        const data = JSON.parse(rawFileContent);
+        const storedPosts = data.posts ?? [];
+        return storedPosts;
+    } catch (error) {
+        if (error.code === "ENOENT") {
+            await fs.writeFileSync(filePath, JSON.stringify({ posts: [] }));
+            return [];
+        } else {
+            throw error;
+        }
+    }
 }
 
 function storePosts(posts) {
     return fs.writeFileSync(
-        "dummy_backend/posts.json",
+        path.join(__dirname, "dummy_backend/posts.json"),
         JSON.stringify({ posts: posts || [] })
     );
 }
