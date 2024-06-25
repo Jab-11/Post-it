a7e7b229da5e22ee60cf5b19133bba415fb349b6
Jabarson Christian <christianjabarson@gmail.com>
2024-06-25 20:05:13 +0530

    commit#20#colorschema

--- post-it/src/components/MainHeader.jsx
+++ post-it/src/components/MainHeader.jsx
@@ -3,20 +3,24 @@ import { Link } from 'react-router-dom';
 
 import classes from './MainHeader.module.css';
 
+
 function MainHeader() {
   return (
-    <header className={classes.header}>
-      <h1 className={classes.logo}>
-        <MdMessage />
-        Post-it
-      </h1>
-      <p>
-        <Link to="/create-post" className={classes.button}>
-          <MdPostAdd size={18} />
-          New Post
-        </Link>
-      </p>
-    </header>
+    <>
+      <header className={classes.header}>
+        <h1 className={classes.logo}>
+          <MdMessage />
+          Post-it
+        </h1>
+        <p>
+          <Link to="/create-post" className={classes.button}>
+            <MdPostAdd size={18} />
+            New Post
+          </Link>
+        </p>
+      </header>
+      <Login />
+    </>
   );
 }
 
--- post-it/src/routes/RootLayout.jsx
+++ post-it/src/routes/RootLayout.jsx
@@ -1,16 +1,13 @@
 import {Outlet} from 'react-router-dom';
 
 import MainHeader from "../components/MainHeader";
-// import Modal from "../components/Modal";
+
 
 function RootLayout(){
     return(
         <>
             <MainHeader />
-            {/* <Modal> */}
-                <Outlet />
-                {/* <Login /> */}
-            {/* </Modal> */}
+            <Outlet />
         </>
     );
 }
